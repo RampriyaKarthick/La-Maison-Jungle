0b34b4b577dd3cd719b854fb5e1a706ec095b6b0
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-05-08 08:22:33 +0200

    la maison jungle initiasation and Banner, Cart

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import Banner from '../components/Banner'
+import Cart from '../components/Cart'
+
+function App() {
+  return (
+  <React.Fragment>
+    <Banner/>
+  <Cart/>
+  </React.Fragment>
+
+   
+  
+  );
+}
+
+export default App;
