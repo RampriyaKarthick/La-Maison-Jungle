e47866fdf292688a2e4fbedce810c0d44f602403
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2024-05-12 07:27:56 +0200

    adding state to update the cart in cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,27 @@
 import '../styles/Cart.css'
+import { useState } from 'react';
+
 function Cart(){
     const monsteraPrice = 8;
-    const ivyPrice= 10
-    const flowerPrice = 15
+    // const ivyPrice= 10
+    // const flowerPrice = 15
+    const[cart,updateCart] = useState(0)
    return(
     <div className='lmj-cart'>
         <h2>Panier</h2>
-        <ul>
-            <li>Monstera: {monsteraPrice}</li>
+        <div>
+        Monstera: {monsteraPrice}€
+        <button onClick={() => updateCart(cart+1)}>
+            Ajouter
+        </button>
+        </div>
+        {/* <ul>
+            <li></li>
             <li>Lierre: {ivyPrice}</li>
             <li>Fleurs: {flowerPrice}</li>
-        </ul>
-        Total: {monsteraPrice + ivyPrice + flowerPrice}€
+        </ul> */}
+
+       <h3> Total:{monsteraPrice * cart}€</h3> 
 
     </div>
    ) 
